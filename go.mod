f9e91763f935d721283bf29165b162a6acba81be
mdnahidhossen1911 <md.nahidhossen1911@gmail.com>
2025-11-12 22:28:09 +0600

    Add initial project files: settings, go.mod, and main.go with product API

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ecom_project
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Product struct {
+	ID          int     `json:"id"` // Adding json tags for better JSON representation
+	Image       string  `json:"image"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
+var ProductsList []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", 400)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(ProductsList)
+}
+
+func init() {
+	fmt.Println("Server is starting...")
+	p1 := Product{
+		ID:          1,
+		Image:       "image1.jpg",
+		Name:        "Product 1",
+		Description: "Description for product 1",
+		Price:       19.99,
+	}
+
+	p2 := Product{
+		ID:          2,
+		Image:       "image2.jpg",
+		Name:        "Product 2",
+		Description: "Description for product 2",
+		Price:       29.99,
+	}
+	p3 := Product{
+		ID:          3,
+		Image:       "image3.jpg",
+		Name:        "Product 3",
+		Description: "Description for product 3",
+		Price:       39.99,
+	}
+
+	p4 := Product{
+		ID:          4,
+		Image:       "image4.jpg",
+		Name:        "Product 4",
+		Description: "Description for product 4",
+		Price:       49.99,
+	}
+
+	ProductsList = []Product{p1, p2, p3, p4}
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/getProducts", getProducts)
+
+	err := http.ListenAndServe(":80", mux)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
